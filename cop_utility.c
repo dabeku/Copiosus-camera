baa12adce0cab39b3b2366a5bdd561fc0ad3e717
dabeku <daniel.kuster@gmail.com>
2019-12-03 15:55:35 +0100

    Fix timestamp

--- cop_utility.c
+++ cop_utility.c
@@ -67,8 +67,8 @@ char* get_timestamp() {
         buffer,
         "%04d-%02d-%02d_%02d-%02d-%02d",
         timeinfo->tm_year + 1900,
-        timeinfo->tm_mday,
         timeinfo->tm_mon + 1,
+        timeinfo->tm_mday,
         timeinfo->tm_hour,
         timeinfo->tm_min,
         timeinfo->tm_sec);
